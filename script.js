d2bb0b29c18bfc424839b44e785a06b53e101e8a
kiri728 <shgssd8y6d@privaterelay.appleid.com>
2026-05-29 16:29:56 +0900

    詳細な予定追加を可能に

--- script.js
+++ script.js
@@ -18,21 +18,24 @@ const schedules = [];
 function addschedule(){
     document.getElementById("schedule-body");
 console.log("1");
-    //imputデータ入手
+    //inputデータ入手
+    const title = document.getElementById("title").value;
     const date = document.getElementById("date").value;
 console.log("2");
-    const time = document.getElementById("time").value;
+    const time1 = document.getElementById("time1").value;
+    const time2 = document.getElementById("time2").value;
 console.log("3");
-    const title = document.getElementById("title").value;
+    
 console.log("4");
     const color = document.getElementById("color").value;
 console.log("5");
 
     //オブジェクト化
     const newSchedule = {
-        date: date,
-        time: time,
         title: title,
+        date: date,
+        time1: time1,
+        time2: time2,
         color: color
     };
 console.log("6");
@@ -45,9 +48,10 @@ console.log("7");
     renderTable();
 console.log("8");
 
-    document.getElementById("date").value = "";
-    document.getElementById("time").value = "";
     document.getElementById("title").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("time1").value = "";
+    document.getElementById("time2").value = "";
     document.getElementById("color").value = "#000000";
 console.log("9");
 }
@@ -68,9 +72,10 @@ console.log("7-2");
     schedules.forEach(schedule => {
         tbody.innerHTML += `
             <tr>
-                <td>${schedule.date}</td>
-                <td>${schedule.time}</td>
                 <td>${schedule.title}</td>
+                <td>${schedule.date}</td>
+                <td>${schedule.time1}</td>
+                <td>${schedule.time2}</td>
                 <td style="background-color:${schedule.color}"></td>
             </tr>
         `;
